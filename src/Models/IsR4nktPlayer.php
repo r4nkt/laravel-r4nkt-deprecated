dc78cb8cfa7a2aaae60277fdeb94e25d8ab0f8f5
Travis Elkins <travis.elkins+github@gmail.com>
2020-06-01 20:42:54 +0200

    Introduced custom_player_id accessor to IsR4nktPlayer trait.

--- src/Models/IsR4nktPlayer.php
+++ src/Models/IsR4nktPlayer.php
@@ -4,6 +4,13 @@ namespace R4nkt\Laravel\Models;
 
 trait IsR4nktPlayer
 {
+    public function getCustomPlayerIdAttribute()
+    {
+        $resolverClass = config('r4nkt.custom_player_id_resolver');
+
+        return (new $resolverClass)->resolve($this);
+    }
+
     /**
      * [scopeCustomPlayerId description]
      * @param  [type] $query          [description]
