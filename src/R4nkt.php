594f878733f3a8b7ebeced326d241106d9db92e2
Travis Elkins <travis.elkins+github@gmail.com>
2020-06-01 16:42:43 +0200

    Shortened reportPlayerActivity helper method to reportActivity.

--- src/R4nkt.php
+++ src/R4nkt.php
@@ -7,7 +7,7 @@ use R4nkt\PhpSdk\R4nkt as PhpR4nkt;
 
 class R4nkt extends PhpR4nkt
 {
-    public function reportPlayerActivity(string $customActionId, int $amount = 1, ?string $session = null)
+    public function reportActivity(string $customActionId, int $amount = 1, ?string $session = null)
     {
         ReportActivity::prep()
             ->byPlayer(auth()->user())
